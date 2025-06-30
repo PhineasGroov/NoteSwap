74f5beec0b6f31c5927fa91ec201d10493baacce
PhineasGroov <phineesnzogue@gmail.com>
2025-06-30 13:30:17 +0200

    add of profile UI and correction of some bugs

--- noteswap/accounts/views.py
+++ noteswap/accounts/views.py
@@ -1,5 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from .models import Profile
+from django.contrib.auth.forms import UserChangeForm
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'avatar']
 
 # Create your views here.
 def register(request):
@@ -11,3 +20,15 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/profile.html', {'form': form, 'profile': profile})
